ad455b7b36b1755c9d95161a4f0e2fa895858bf0
Mohammad Fazel Piri <piri@mahsan.co>
2022-09-09 15:21:46 +0430

    Handle fetched data inside activity

--- app/src/main/java/com/faz/news3/ui/news/NewsListFragment.kt
+++ app/src/main/java/com/faz/news3/ui/news/NewsListFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.faz.news3.R
+import com.faz.news3.domain.model.NewsArticle
+import com.faz.news3.ui.state.UiState
 import kotlinx.coroutines.launch
 
 class NewsListFragment : Fragment(R.layout.fragment_news) {
@@ -20,12 +23,26 @@ class NewsListFragment : Fragment(R.layout.fragment_news) {
         with(viewModel) {
             loadNews(1)
             viewLifecycleOwner.lifecycleScope.launch {
-                newsFlow.collect {
-                    // display ui!
+                newsFlow.collect { uiState ->
+                    when (uiState) {
+                        is UiState.Success -> displayNewsOnUi(uiState.data)
+                        is UiState.Error -> showError(uiState.errorMessage)
+                        else -> {
+                            Unit
+                        }
+                    }
                 }
             }
         }
 
         return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    private fun showError(errorMessage: String) {
+        Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show()
+    }
+
+    private fun displayNewsOnUi(data: List<NewsArticle>) {
+        // TODO
     }
 }
